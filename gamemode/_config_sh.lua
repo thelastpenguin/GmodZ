9095a9be15d5aa6b486e39457f948563fa60ce27
thelastpenguin <thelastpenguin212@gmail.com>
2014-03-18 00:29:03 -0700

    Fixed bug with lootmanager

--- gamemode/_config_sh.lua
+++ gamemode/_config_sh.lua
@@ -6,8 +6,8 @@ gmodz.cfg.max_water = 100;
 
 gmodz.cfg.item_despawn = 300; -- wait five minutes.
 
-gmodz.cfg.loot_respawnTime = 10;
-gmodz.cfg.loot_respawnVariance = 2;
+gmodz.cfg.loot_respawnTime = 200;
+gmodz.cfg.loot_respawnVariance = 100;
 
 gmodz.cfg.user_invSize = {w=5,h=6};
 gmodz.cfg.user_bankSize = {w=4,h=6};

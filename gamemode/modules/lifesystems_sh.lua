d315ebf1b8db9b7c85088af816458ee14091575c
thelastpenguin <thelastpenguin212@gmail.com>
2014-03-19 08:09:54 -0700

    Fixed _gmodz_drinkwater exploit
    Made it so you had to actually be in water for the _gmodz_drinkwater command to run when the +use bind is pressed. Previously it just ran regardless of water presence.

--- gamemode/modules/lifesystems_sh.lua
+++ gamemode/modules/lifesystems_sh.lua
@@ -23,12 +23,17 @@ if SERVER then
 		end
 	end);
 	
-	-- THESE GET RESET ON SPAWN
+	-- 
+	-- REFIL LIFE RESOURCES ON PLAYER SPAWN. 
+	--	note: this needs to be prevented from overriding mysql loaded data.
 	gmodz.hook.Add( 'PlayerSpawn', function( pl )
 		pl:SetUData( 'water', gmodz.cfg.max_water );
 		pl:SetUData( 'food', gmodz.cfg.max_food );
 	end);
 	
+	--
+	-- CONSOLE COMMAND TO DRINK WATER
+	--
 	concommand.Add( '_gmodz_drinkwater', function( pl )
 		if pl:WaterLevel() >= 1 then
 			pl:SetUData( 'water', gmodz.cfg.max_water );
@@ -36,15 +41,22 @@ if SERVER then
 	end);
 
 else
+	local LocalPlayer = _G.LocalPlayer ;
 	-- PROMPT
+	local function canDrink( )
+		return LocalPlayer:WaterLevel() >= 1 ;
+	end
+	
+	-- HUD PROMPT: Drink Water
 	gmodz.hook.Add( 'hud_prompt', function()
-		if LocalPlayer():WaterLevel() >= 1 then
+		if canDrink() then
 			return 'Press \'E\' to drink water';
 		end
 	end);
 	
+	-- BIND DETECT: Drink Water
 	gmodz.hook.Add('PlayerBindPress', function( pl, bind )
-		if bind == '+use' then
+		if bind == '+use' and canDrink() then
 			LocalPlayer():ConCommand( '_gmodz_drinkwater\n' );
 		end
 	end);

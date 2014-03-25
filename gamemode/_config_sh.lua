7ab6243f878dfd69e3c9586aa903a802f38b921f
thelastpenguin <thelastpenguin212@gmail.com>
2014-03-25 00:11:46 -0700

    Added new config setting

--- gamemode/_config_sh.lua
+++ gamemode/_config_sh.lua
@@ -1,5 +1,6 @@
 gmodz.cfg = {};
 
+
 gmodz.cfg.starting_health = 100;
 gmodz.cfg.max_food = 200;
 gmodz.cfg.max_water = 100;
@@ -16,3 +17,7 @@ gmodz.cfg.user_bankSize = {w=5,h=6};
 
 gmodz.cfg.color_highlight = Color(255,150,0);
 gmodz.cfg.color_darkbg = Color(50,54,50,230);
+
+
+-- DEBUG SETTINGS
+gmodz.cfg.d_loadrate = 2;
